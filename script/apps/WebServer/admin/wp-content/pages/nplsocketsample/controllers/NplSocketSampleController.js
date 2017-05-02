e3b4192d4a17da3ed9954d3097b976d1ffebde21
zhangleio <zhangleio@outlook.com>
2017-05-02 17:35:29 +0800

    test

--- script/apps/WebServer/admin/wp-content/pages/nplsocketsample/controllers/NplSocketSampleController.js
+++ script/apps/WebServer/admin/wp-content/pages/nplsocketsample/controllers/NplSocketSampleController.js
@@ -2,16 +2,16 @@ var m = angular.module('NplWebSocketSample_App', ['ui.bootstrap']);
 m.component("customcomponent", {
     templateUrl: "/wp-content/pages/nplsocketsample/templates/NplSocketSampleTemplate.html",
     controller: function ($scope, $http, $log) {
-
+        $scope.is_connected = false;
+        $scope.index = 0;
         $scope.onConnect = function () {
 
             // Let us open a web socket
-            var ws = new WebSocket("ws://localhost:60001");
+            var ws = new WebSocket("ws://localhost:8099/ajax/nplsocketsample?action=handshake");
 
             ws.onopen = function () {
-                // Web Socket is connected, send data using send()
-                ws.send("Message to send");
-                console.log("Message is sent...");
+                ws.send("Hello World");
+                $scope.is_connected = true;
             };
 
             ws.onmessage = function (evt) {
@@ -26,6 +26,14 @@ m.component("customcomponent", {
             ws.onerror = function (e) {
                 console.log("onerror:",e);
             };
+            $scope.ws = ws;
+        }
+
+        $scope.onSend = function () {
+            if ($scope.ws && $scope.is_connected) {
+                $scope.index++;
+                $scope.ws.send("Hello World " + $scope.index);
+            }
         }
     }
 
